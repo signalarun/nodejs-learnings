6cb753ae292bb04c3d30cf697d554fc01d9beb1e
Arun Gopi <signalarun@gmail.com>
2020-06-29 12:05:11 +0530

    Update app.js

--- samples/NodeJsWebApplication/app.js
+++ samples/NodeJsWebApplication/app.js
@@ -44,7 +44,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler, an error handling middleware
 app.use(function(err, req, res, next) {
     
   // set locals, only providing error in development
