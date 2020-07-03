947cd8a348ae1aee458a5d09ff104bb2c1306c43
signalarun <signalarun@gmail.com>
2020-07-03 12:34:58 +0530

    update : sample users

--- samples/NodeJsWebApplication/app/model/user.js
+++ samples/NodeJsWebApplication/app/model/user.js
@@ -5,3 +5,28 @@
  */
 
 import { db } from './connect';
+
+
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
