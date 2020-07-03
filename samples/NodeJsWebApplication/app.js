0e208c8e075e490ebc57b3092047462da2de810d
signalarun <signalarun@gmail.com>
2020-07-03 16:54:28 +0530

    update : 404 error handling

--- samples/NodeJsWebApplication/app.js
+++ samples/NodeJsWebApplication/app.js
@@ -13,6 +13,52 @@ var layouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var apiRouter   = require('./routes/api');
+var cUser = require('./app/controller/user');
+
+/* Authentication */
+passport.use(new LocalStrategy(
+        function (username, password, done) {
+            // asynchronous verification, for effect...
+            process.nextTick(function () {
+
+                // Find the user by username.  If there is no user with the given
+                // username, or the password is not correct, set the user to `false` to
+                // indicate failure and set a flash message.  Otherwise, return the
+                // authenticated `user`.
+                cUser.findByUsername(username, function (err, user) {
+                    if (err) {
+                        return done(err);
+                    }
+                    if (!user) {
+                        return done(null, false, {message: 'Incorrect username.'});
+                    }
+                    if (user.password !== password) {
+                        return done(null, false, {message: 'Incorrect password.'});
+                    }
+                    return done(null, user); //  user found valid
+                });
+            });
+        }
+));
+
+/*
+ In a typical web application, the credentials used to authenticate a user will
+ only be transmitted during the login request. If authentication succeeds, 
+ a session will be established and maintained via a cookie set in the user's browser.
+ Each subsequent request will not contain credentials, but rather the unique cookie 
+ that identifies the session. In order to support login sessions, Passport will 
+ serialize and deserialize user instances to and from the session. 
+ Ref. http://www.passportjs.org/docs/authenticate/
+ */
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  cUser.findById(id, function(err, user) {
+    done(err, user);
+  });
+});  
 
 
 var app = express();
@@ -30,6 +76,13 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public'))); 
 app.use(express.static(path.join(__dirname, 'public'))); // Setting up public directories
 
+// TODO remove if session not required
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
 /*
 Middleware to print requests
 app.use((req, res, next)=>{
@@ -44,7 +97,8 @@ app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.status(404).render("404");
+  //  next(createError(404));
 });
 
 // error handler, an error handling middleware
