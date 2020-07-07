d84fa8768cd830bf044058e2a8ebe56d0bc7e3c0
signalarun <signalarun@gmail.com>
2020-07-07 11:28:50 +0530

    update : login to access index

--- samples/NodeJsWebApplication/routes/index.js
+++ samples/NodeJsWebApplication/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', require('connect-ensure-login').ensureLoggedIn('/api/account/authentication/v1/login'), function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
