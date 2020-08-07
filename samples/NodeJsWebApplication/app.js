f0c7073f2a69fa7789c3902d0ce2402257bbb2d1
signalarun <signalarun@gmail.com>
2020-08-07 16:35:58 +0530

    update : Moved passport related code from app.js

--- samples/NodeJsWebApplication/app.js
+++ samples/NodeJsWebApplication/app.js
@@ -1,6 +1,4 @@
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var passport = require('./config/passport');
 var createError = require('http-errors');
 var flash = require('connect-flash');
 // Web framework
@@ -14,52 +12,6 @@ var layouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var apiRouter   = require('./routes/api');
-var cUser = require('./app/controller/user');
-
-/* Authentication */
-passport.use(new LocalStrategy(
-        function (username, password, done) {
-            // asynchronous verification, for effect...
-            process.nextTick(function () {
-
-                // Find the user by username.  If there is no user with the given
-                // username, or the password is not correct, set the user to `false` to
-                // indicate failure and set a flash message.  Otherwise, return the
-                // authenticated `user`.
-                cUser.findByUsername(username, function (err, user) {
-                    if (err) {
-                        return done(err);
-                    }
-                    if (!user) {
-                        return done(null, false, {message: 'Incorrect username.'});
-                    }
-                    if (user.password !== password) {
-                        return done(null, false, {message: 'Incorrect password.'});
-                    }
-                    return done(null, user); //  user found valid
-                });
-            });
-        }
-));
-
-/*
- In a typical web application, the credentials used to authenticate a user will
- only be transmitted during the login request. If authentication succeeds, 
- a session will be established and maintained via a cookie set in the user's browser.
- Each subsequent request will not contain credentials, but rather the unique cookie 
- that identifies the session. In order to support login sessions, Passport will 
- serialize and deserialize user instances to and from the session. 
- Ref. http://www.passportjs.org/docs/authenticate/
- */
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  cUser.findById(id, function(err, user) {
-    done(err, user);
-  });
-});  
 
 var app = express();
 
@@ -81,8 +33,8 @@ app.use(require('express-session')({ secret: 'keyboard cat', resave: false, save
 app.use(flash());
 // Initialize Passport and restore authentication state, if any, from the
 // session.
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.passport.initialize());
+app.use(passport.passport.session());
 
 /*
 Middleware to print requests
