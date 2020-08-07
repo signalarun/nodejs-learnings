b529f6c54663fe187d2e5d29acb0928390f1cd1e
signalarun <signalarun@gmail.com>
2020-08-07 17:57:24 +0530

    update : documentation

--- samples/NodeJsWebApplication/config/password.js
+++ samples/NodeJsWebApplication/config/password.js
@@ -7,6 +7,12 @@
 var crypto = require('crypto');
 var scmp = require('scmp');
 
+/**
+ * The function first uses randomBytes that will return 256 random bytes of data.
+ * Then take that as salt and the clear text password and send it to Password-Based Key Derivation Function 2 (PBKDF2), 
+ * which is a function that will create a derived key based on salt, our password, and a work factor. 
+ * We then return salt and the derived key back so that we can persist it.
+ */
 var encryptPassword = function passwordCreate(password, cb){
   crypto.randomBytes(process.env.CRYPTO_RANDOMSIZE, function(err, salt){
     if (err)
