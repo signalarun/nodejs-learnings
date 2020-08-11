7b30f4c6e57fbc71f15d330f3259632f8af9fd55
signalarun <signalarun@gmail.com>
2020-08-11 16:37:01 +0530

    update : add user, update user

--- samples/NodeJsWebApplication/app/model/user.js
+++ samples/NodeJsWebApplication/app/model/user.js
@@ -8,48 +8,86 @@
 
 const passwordUtil = require('../../config/passport');
 
-var users = [
-    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
-  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
-  , { id: 3, username: 'aryastark@gmail.com', password: 'password', email: 'aryastark@gmail.com' }
-];
+/*var users = [
+ {id: 1, username: 'bob', password: 'secret', email: 'bob@example.com'}
+ , {id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com'}
+ , {id: 3, username: 'aryastark@gmail.com', password: 'password', email: 'aryastark@gmail.com'}
+ ];*/
 
-/*
-const dUsers = {
-    josh :{
+const users = {
+    josh: {
         salt: 'G81lJERghovMoUX5+RoasvwT7evsK1QTL33jc5pjG0w=',
         password: 'DAq+sDiEbIR0fHnbzgKQCOJ9siV5CL6FmXKAI6mX7UY=',
         work: 5000,
         displayName: 'Josh',
         id: 'josh',
         provider: 'local',
-        username: 'josh'    
+        username: 'josh'
+    }
+};
+
+/*
+exports.findById = function (id, fn) {
+    var idx = id - 1;
+    if (users[idx]) {
+        fn(null, users[idx]);
+    } else {
+        fn(new Error('User ' + id + ' does not exist'));
     }
 };*/
 
-exports.findById = function(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
+exports.findById = function (id, cb) {
+
+    if (users[id]) {
+        cb(null, users[id]);
+    } else {
+        cb(new Error('User : ' + id + ' does not exist'));
+    } 
 };
 
-exports.findByUsername = function(username, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
+/*
+exports.findByUsername = function (username, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+        var user = users[i];
+        if (user.username === username) {
+            return fn(null, user);
+        }
     }
-  }
-  return fn(null, null);
+    return fn(null, null);
+};
+*/
+
+exports.findByUsername = function (username, cb) {
+    if (users[username]) {
+        cb(null, users[username]);
+    } else {
+        cb(new Error('User : ' + username + ' does not exist'));
+    }
 };
 
-exports.addUser = function(username, password, work, cb){
-    // TODO addUser
+exports.addUser = function (username, password, work, cb) {
+    if (users[username] === undefined) {
+        passwordUtil.passwordCreate(password, function (err, salt, password) {
+            users[username] = {
+                salt: salt,
+                password: password,
+                work: work,
+                displayName: username,
+                id: username,
+                provider: 'local',
+                username: username
+            };
+            return cb(null, users[username]);
+        });
+    } else {
+        return cb({errorCode: 1, message: 'User exists!'}, 'User exists!', null);
+    }
 };
 
-exports.updatePassword = function(username, password){
-    // TODO updatePassword
+exports.updatePassword = function (username, password, work) {
+    passwordUtil.passwordCreate(password, function (err, salt, password) {
+        users[username].salt = salt;
+        users[username].password = password;
+        users[username].work = work;
+    });
 };
