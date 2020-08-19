f3e0efb5ceaaefc9d8caae8db661dd2798c3d19c
signalarun <signalarun@gmail.com>
2020-08-19 14:51:15 +0530

    new import : mongodb models

---
+++ samples/NodeJsWebApplication/app/model/mongodb/user.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+let mongoose = require('mongoose');
+
+let userSchema = new mongoose.Schema({
+    salt: String,
+    password: String,
+    work: String,
+    firstName: String,
+    lastName: String,
+    displayName: String,
+    id: String,
+    provider: String,
+    username: String
+});
+
+module.exports = mongoose.model('User', userSchema);
