541edcab29658cf1100f2f21b7b65c4ad6643bbf
signalarun <signalarun@gmail.com>
2021-04-08 16:43:15 +0530

    update : Googel oauth configuation with passport

--- samples/RestApiOAuth/server.js
+++ samples/RestApiOAuth/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const cookieSession = require('cookie-session');
+const chalk = require('chalk');
+const config = require('./config');
 
 const app = express();
 const PORT = process.env.port || 3000;
@@ -12,8 +14,25 @@ app.use(cookieSession({ maxAge: 60 * 1000, keys: ['1234'] }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Strategy configurations
+passport.use(new GoogleStrategy({clientID : config.oauthConfig.Google.clientId, 
+    clientSecret : config.oauthConfig.Google.clientSecret,
+    callbackURL : 'http://localhost:3000/auth/google/callback'}, (accessToken, refresstoken, profile, done) => {
+        done(null, profile);
+    } ));
+
+// Used to add a pieceof information into cookie    
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+// Used to decode the recieved cookie and persist the session
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
 var route = require('./api/route/note');
 route(app);
 
 console.log("App started on port ", PORT);
-console.log("I am Waiting for requests..");
+console.log(chalk.bold.green("I am Waiting for requests.."));
